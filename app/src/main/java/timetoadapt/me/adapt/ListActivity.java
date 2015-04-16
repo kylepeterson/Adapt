dca1d705703c1a5e19e315c0bfd0caa0793f1c6f
Kyle Peterson <peterson.kyle9@gmail.com>
2015-04-15 23:40:51 -0700

    fragments are working for the category page and the hypothesis list page. Once we get some dummy data I should be able to get the hyp list page populating

--- app/src/main/java/timetoadapt/me/adapt/ListActivity.java
+++ app/src/main/java/timetoadapt/me/adapt/ListActivity.java
@@ -1,9 +1,15 @@
 package timetoadapt.me.adapt;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 
 public class ListActivity extends Activity {
@@ -12,8 +18,18 @@ public class ListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-    }
 
+        if(savedInstanceState == null) {
+            // When entered through browse button
+            // Create new categories fragment
+            CategoriesFragment topic = new CategoriesFragment();
+            topic.setArguments(getIntent().getExtras());
+            // Inflate categories overview fragment
+            getFragmentManager().beginTransaction().replace(R.id.container, topic).commit();
+            // Put code here to access server to fill in appropriate category information
+            // ...
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +52,65 @@ public class ListActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Fragment representing the overview of the categories of hypotheses
+    // For now this is just sleep, focus and nutrition
+    public static class CategoriesFragment extends Fragment {
+
+        public CategoriesFragment() {
+
+        }
+
+        public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Set layout to category fragment
+            View rootView = inflater.inflate(R.layout.categories_fragment, container, false);
+            // Grab category buttons from layout
+            final Button category1 = (Button) rootView.findViewById(R.id.cat1);
+            final Button category2 = (Button) rootView.findViewById(R.id.cat2);
+            final Button category3 = (Button) rootView.findViewById(R.id.cat3);
+            // Generalized click listener for all three buttons
+            View.OnClickListener clickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create new list fragment
+                    HypothesisListFragment list = new HypothesisListFragment();
+                    // Attach name of category to fragment
+                    Bundle nextArgs = new Bundle();
+                    Button currentButton = (Button) v;
+                    nextArgs.putString("category", currentButton.getText().toString());
+                    list.setArguments(nextArgs);
+                    // Inflate list fragment
+                    getFragmentManager().beginTransaction().replace(R.id.container, list).commit();
+                }
+            };
+            // Set click listeners to all three buttons
+            category1.setOnClickListener(clickListener);
+            category2.setOnClickListener(clickListener);
+            category3.setOnClickListener(clickListener);
+            return rootView;
+        }
+    }
+
+    // Fragment for the list of Hypothesis reached when a category is clicked
+    // or a hypothesis is searched for
+    public static class HypothesisListFragment extends Fragment {
+
+        public HypothesisListFragment() {
+
+        }
+
+        public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.hypothesis_list_fragment, container, false);
+            Bundle arguments = getArguments();
+            String category = arguments.getString("category");
+            // put code here to query parse for all hypothesis mapping to the category
+            ListView list = (ListView) rootView.findViewById(R.id.hypList);
+            // put code here to append rows to the list view for each hypothesis
+            // needs to use ArrayAdapter and a custom layout for each row, found in hypothesis_row.xml
+            return rootView;
+        }
+    }
+
 }
