b6c7d954dc691e9e2511d5c4f5d1e15a24ee5d3f
Kyle Peterson <peterson.kyle9@gmail.com>
2015-05-01 15:41:16 -0700

    big change baby

--- app/src/main/java/timetoadapt/me/adapt/MainActivity.java
+++ app/src/main/java/timetoadapt/me/adapt/MainActivity.java
@@ -154,7 +154,7 @@ public class MainActivity extends Activity {
             });
 
             ((TextView) rootView.findViewById(R.id.user_creation_title)).setText(R.string.create_user_welcome);
-            ((TextView) rootView.findViewById(R.id.user_creation_explanation_text)).setText(R.string.hypothesis_subscription_explanation);
+            //((TextView) rootView.findViewById(R.id.user_creation_explanation_text)).setText(R.string.hypothesis_subscription_explanation);
 
             return rootView;
         }
