92ec4796d5a400ac780b5d777d23b510662e3773
Kyle Peterson <peterson.kyle9@gmail.com>
2015-05-06 12:28:47 -0700

    added logo and color to splash screen

--- app/src/main/java/timetoadapt/me/adapt/AdaptSplash.java
+++ app/src/main/java/timetoadapt/me/adapt/AdaptSplash.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class Splash extends Activity {
+public class AdaptSplash extends Activity {
 
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 1000;
@@ -15,16 +15,15 @@ public class Splash extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.splash);
-
         /* New Handler to start the Menu-Activity 
          * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
-                Splash.this.startActivity(mainIntent);
-                Splash.this.finish();
+                Intent mainIntent = new Intent(AdaptSplash.this, MainActivity.class);
+                AdaptSplash.this.startActivity(mainIntent);
+                AdaptSplash.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
