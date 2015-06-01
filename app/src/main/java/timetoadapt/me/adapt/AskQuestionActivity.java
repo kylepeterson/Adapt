f2b64a74102978ea9189aa23b69668d8e6584e1c
Kyle Peterson <peterson.kyle9@gmail.com>
2015-06-01 16:52:57 -0700

    action bar now toggles between log in and log out depending on user status

--- app/src/main/java/timetoadapt/me/adapt/AskQuestionActivity.java
+++ app/src/main/java/timetoadapt/me/adapt/AskQuestionActivity.java
@@ -210,6 +210,17 @@ public class AskQuestionActivity extends Activity {
         // Assumes current activity is the searchable activity
         searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, ListActivity.class)));
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        // log in vs log out
+        if(instance.getCurrentUser() == null) {
+            // not logged in
+            menu.findItem(R.id.action_log_in).setVisible(true);
+            menu.findItem(R.id.action_log_out).setVisible(false);
+        } else {
+            // logged in
+            menu.findItem(R.id.action_log_in).setVisible(false);
+            menu.findItem(R.id.action_log_out).setVisible(true);
+        }
         return true;
     }
 
@@ -230,6 +241,10 @@ public class AskQuestionActivity extends Activity {
                 instance.logoutCurrentUser();
                 startActivity(new Intent(AskQuestionActivity.this, MainActivity.class));
                 Log.d("actionbar", "logout clicked");
+                return true;
+            case R.id.action_log_in:
+                final Intent signInActivity = new Intent(AskQuestionActivity.this, SignInActivity.class);
+                startActivity(signInActivity);
                 return true;
         }
 
--- app/src/main/java/timetoadapt/me/adapt/MainActivity.java
+++ app/src/main/java/timetoadapt/me/adapt/MainActivity.java
@@ -187,6 +187,17 @@ public class MainActivity extends Activity {
         // Assumes current activity is the searchable activity
         searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, ListActivity.class)));
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        // log in vs log out
+        if(instance.getCurrentUser() == null) {
+            // not logged in
+            menu.findItem(R.id.action_log_in).setVisible(true);
+            menu.findItem(R.id.action_log_out).setVisible(false);
+        } else {
+            // logged in
+            menu.findItem(R.id.action_log_in).setVisible(false);
+            menu.findItem(R.id.action_log_out).setVisible(true);
+        }
         return true;
 
 
@@ -209,6 +220,10 @@ public class MainActivity extends Activity {
                 instance.logoutCurrentUser();
                 startActivity(new Intent(MainActivity.this, MainActivity.class));
                 Log.d("actionbar", "logout clicked");
+                return true;
+            case R.id.action_log_in:
+                final Intent signInActivity = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(signInActivity);
                 return true;
         }
 
